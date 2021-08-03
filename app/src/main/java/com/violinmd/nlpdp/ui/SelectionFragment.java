aeeeb3e5b69a15b9722ae4f0a4f07ea17dfa5e8a
Allen Chang <allenchangmusic@outlook.com>
2021-08-03 13:27:40 -0230

    Migrate spinners and editText to Material View

--- app/src/main/java/com/violinmd/nlpdp/ui/SelectionFragment.java
+++ app/src/main/java/com/violinmd/nlpdp/ui/SelectionFragment.java
@@ -4,11 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.LinearLayout;
-import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -22,8 +21,6 @@ import com.violinmd.nlpdp.R;
 import com.violinmd.nlpdp.RecyclerItem.RecyclerItem;
 import com.violinmd.nlpdp.RecyclerItem.RecyclerViewAdapter;
 
-import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,68 +29,34 @@ public class SelectionFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_selection, container, false);
-        Spinner spinner = root.findViewById(R.id.editTextselection);
-        final String[] arraySpinner = new String[NLPDP.options.size() + 1];
-        arraySpinner[NLPDP.options.size()] = "Select a Medication";
-        for (int i = 0; i < NLPDP.options.size(); i++) {
-            arraySpinner[i] = NLPDP.options.get(i);
-        }
-        getActivity().runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                ArrayAdapter<String> adapter = new ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_dropdown_item, arraySpinner) {
-                    @Override
-                    public View getDropDownView(int position, View convertView, @NotNull ViewGroup parent) {
-                        return super.getDropDownView(position, convertView, parent);
-                    }
-
-                    @Override
-                    public int getCount() {
-                        return arraySpinner.length - 1;
-                    }
-                };
-                adapter.setDropDownViewResource(R.layout.layout_simplespinner);
-                spinner.setAdapter(adapter);
-                spinner.setSelection(arraySpinner.length - 1);
-                spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-                    @Override
-                    public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
-                    }
-
-                    @Override
-                    public void onNothingSelected(AdapterView<?> parentView) {
-                    }
-                });
-
-            }
-        });
+        AutoCompleteTextView spinner = root.findViewById(R.id.editTextselection);
+        ArrayAdapter<String> statusAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_dropdown_item, NLPDP.options);
+        spinner.setAdapter(statusAdapter);
 
         Button button = root.findViewById(R.id.button_selection);
 
         button.setOnClickListener(v -> {
-            if(spinner.getSelectedItemPosition()!=spinner.getCount()){
-                Thread network = new Thread() {
-                    public void run() {
-                        ArrayList<Medication> meds = NLPDP.NLPDPsearchSelect(spinner.getSelectedItem().toString().replaceAll(" ","+"));
-                        if(meds!=null){
+            Thread network = new Thread() {
+                public void run() {
+                    ArrayList<Medication> meds = NLPDP.NLPDPsearchSelect(spinner.getText().toString().replaceAll(" ","+"));
+                    if(meds!=null){
 
-                            getActivity().runOnUiThread(() -> {
-                                LinearLayout scrollView = root.findViewById(R.id.scroll_select);
-                                scrollView.removeAllViews();
+                        requireActivity().runOnUiThread(() -> {
+                            LinearLayout scrollView = root.findViewById(R.id.scroll_select);
+                            scrollView.removeAllViews();
 
-                                MultiLevelRecyclerView multiLevelRecyclerView = new MultiLevelRecyclerView(getContext());
-                                multiLevelRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-                                List<RecyclerViewItem> itemList = recursivePopulate1(meds);
-                                RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(getContext(), itemList);
-                                multiLevelRecyclerView.setAdapter(myAdapter);
-                                scrollView.addView(multiLevelRecyclerView);
-                            });
-                        }
-
+                            MultiLevelRecyclerView multiLevelRecyclerView = new MultiLevelRecyclerView(requireContext());
+                            multiLevelRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                            List<RecyclerViewItem> itemList = recursivePopulate1(meds);
+                            RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(getContext(), itemList);
+                            multiLevelRecyclerView.setAdapter(myAdapter);
+                            scrollView.addView(multiLevelRecyclerView);
+                        });
                     }
-                };
-                network.start();
-            }
+
+                }
+            };
+            network.start();
         });
 
         return root;
